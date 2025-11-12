f53dfa79c66b6f788f56dda8661145ce5f38333a
faltantornillos <faltantornillos@gmail.com>
2025-11-12 09:02:56 -0800

    better UI

--- app/src/main/java/com/example/icscalendar/MainActivity.kt
+++ app/src/main/java/com/example/icscalendar/MainActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
